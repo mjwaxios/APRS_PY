8bdb66ef974bdbd4bb21a9fb01c88ed81a93d416
mwyrick <mjw@axiosengineering.com>
2017-04-24 10:43:07 -0400

    Moved filter to 0,0 and range the entire earth

--- aprs.py
+++ aprs.py
@@ -4,7 +4,7 @@ import aprs
 def p(x): 
 	print "Source: %-10s Dest: %-10s Path: %-50s Text: %s" % (x.source, x.destination, x.path, x.text)
 
-a = aprs.TCP('nocall', '-1', None, 'r/38.982/-77.439/120')
+a = aprs.TCP('nocall', '-1', None, 'r/0/0/65535')
 a.start()
 
 a.receive(callback=p)
